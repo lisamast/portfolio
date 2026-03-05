c005bb257efddb4b447068e7a1c9890401b72cf9
lisamast <9027431@student.zadkine.nl>
2026-03-05 17:50:41 +0100

    email koppelen contact formulier

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,19 +1,54 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import InputField from "./InputField";
 
 const ContactForm = () => {
+
+    const form = useRef();
+    const [success, setSuccess] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        console.log("form submitted");
+
+        emailjs.sendForm(
+            "service_xf87jjj",
+            "template_31juooc",
+            form.current,
+            "3W57RyjQnm7-2e-hS"
+        )
+        .then(() => {
+            setSuccess(true);
+            e.target.reset();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
     return ( 
-        <form className="contact-form" action="">
+        <form ref={form} onSubmit={sendEmail} className="contact-form">
+
+            <input type="hidden" name="_captcha" value="false"></input>
          
-            <InputField type="text" label="Name" id="name" />
-            <InputField type="email" label="Email" id="email" />
-            <InputField type="text" label="Phonenumber" id="phonenumber" />
+            <InputField type="text" label="Name" id="name" name="name" />
+            <InputField type="email" label="Email" id="email" name="email" />
+            <InputField type="text" label="Phonenumber" id="phonenumber" name="phonenumber" />
             
-            <label htmlFor="Message" className="contact-label">Message</label>
-            <textarea type="text" label="Message" id="message" className="contact-textarea"></textarea>
+            <label htmlFor="message" className="contact-label">Message</label>
+            <textarea
+                name="message"
+                id="message"
+                className="contact-textarea"
+                required
+            ></textarea>
 
             <button type="submit" className="btn">Send</button>
+
+            {success && <p className="success-message">Message sent successfully!</p>}
         </form>
      );
 }
- 
+
 export default ContactForm;
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,8 +1,8 @@
-const InputField = ({type, label, id}) => {
+const InputField = ({type, label, id, name}) => {
     return ( 
         <div className="input-field">
             <label htmlFor={id} className="input-label">{label}</label>
-            <input type={type} id={id} className="input-box" />
+            <input type={type} id={id} name={name} className="input-box" required/>
         </div>
      );
 }
